09bfb10c2ee47c7ec1051b3f3b738c907890d978
Morgawr <morgawr@gmail.com>
2014-01-25 23:08:20 +0100

    prototype working

--- src/lt/plugins/color_picker.cljs
+++ src/lt/plugins/color_picker.cljs
@@ -1,29 +1,64 @@
 (ns lt.plugins.color-picker
   (:require [lt.object :as object]
             [lt.objs.tabs :as tabs]
-            [lt.objs.command :as cmd])
+            [lt.objs.command :as cmd]
+            [lt.objs.notifos :as notifos]
+            [lt.objs.editor :as editor]
+            [lt.objs.editor.pool :as pool]
+            [lt.util.js :as util])
+  (:use [lt.util.cljs :only [js->clj]])
   (:require-macros [lt.macros :refer [defui behavior]]))
 
-(defui hello-panel [this]
-  [:h1 "Hello from color-picker"])
 
-(object/object* ::color-picker.hello
-                :tags [:color-picker.hello]
-                :name "color-picker"
-                :init (fn [this]
-                        (hello-panel this)))
+(defn add-color-to-line [this color]
+  (editor/insert-at-cursor this color))
 
-(behavior ::on-close-destroy
-          :triggers #{:close}
-          :reaction (fn [this]
-                      (when-let [ts (:lt.objs.tabs/tabset @this)]
-                        (when (= (count (:objs @ts)) 1)
-                          (tabs/rem-tabset ts)))
-                      (object/raise this :destroy)))
+(defn x-offset [cursor]
+  ((js->clj cursor) "left"))
 
-(def hello (object/create ::color-picker.hello))
+(defui color-wheel [this line cursor]
+  [:div {:style (str "width: 200px; margin-left: " (x-offset cursor)) }
+   [:input {:type "color"
+            :style "width: 100%; height: 50px"}]]
+  :change (fn [event]
+            (let [color (.-value (.-target event))]
+              (object/raise this :close-picker)
+              (add-color-to-line this color))))
 
-(cmd/command {:command ::say-hello
-              :desc "color-picker: Say Hello"
+
+(object/behavior* ::color-picker-open
+                  :type :user
+                  :desc "Open the color picker"
+                  :triggers #{:open-picker}
+                  :reaction (fn [this]
+                              (editor/operation (editor/->cm-ed this)
+                                                (fn []
+                                                  (let [prev (.getScrollInfo (editor/->cm-ed this))
+                                                        {:keys [line]} (editor/->cursor this)
+                                                        coords (.cursorCoords (editor/->cm-ed this))]
+                                                    (when-let [picker (:color-picker @this)]
+                                                      (editor/remove-line-widget this picker))
+                                                    (object/merge! this
+                                                                   {:color-picker (editor/line-widget this
+                                                                                                      line
+                                                                                                      (color-wheel this line coords))}))))))
+
+(object/behavior* ::color-picker-close
+                  :type :user
+                  :desc "Close the color picker"
+                  :triggers #{:close-picker :move}
+                  :reaction (fn [this]
+                              (editor/operation (editor/->cm-ed this)
+                                                (fn []
+                                                  (when-let [picker (:color-picker @this)]
+                                                    (editor/remove-line-widget this picker))))))
+
+(cmd/command {:command ::color-picker-open
+              :desc "Open the color picker"
               :exec (fn []
-                      (tabs/add-or-focus! hello))})
+                      (when-let [ed (pool/last-active)]
+                        (object/raise ed :open-picker)))})
+
+(cmd/command {:command ::color-picker-close
+              :desc "Close the currently open color picker"
+              :exec (partial close-picker) })
